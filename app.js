44a26f7fa0dcda3674f3a1d6bc3410c09b7fefa4
dustinheestand <heestand@post.harvard.edu>
2018-08-03 10:12:47 -0500

    deleted views

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
+const router = require('./routes');
 
-app.use('/');
+app.use(morgan('dev'));
+app.use(express.json({}));
+app.use(express.urlencoded({ extended: false }));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(router);
+
+if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
+
+module.exports = app; // this line is only used to make testing easier.
