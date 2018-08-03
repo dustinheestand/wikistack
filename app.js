4db58149c44bbfe1e676640e08f879ba021b619e
dustinheestand <heestand@post.harvard.edu>
2018-08-03 10:46:11 -0500

    added db

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const router = require('./routes');
+const { db } = require('./models');
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
 
 app.use(morgan('dev'));
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(router);
-
-
 
 if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
 
