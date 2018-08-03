22828397a36da7e0b31cd091fe64c9173ce3e772
mnoesen <mnoesen@users.noreply.github.com>
2018-08-03 15:02:56 -0500

    oiwn

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const views = require ('../views');
+const { Page } = require('../models');
 
-
-router.get('/', (req, res, next) => {
-  res.send(views.main(""));
+router.get('/', async (req, res, next) => {
+  const allPages = await Page.findAll();
+  console.log(allPages);
+  res.send(views.main(allPages));
 });
 
 module.exports = router
